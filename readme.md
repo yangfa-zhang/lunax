6a115729e7793e337ba1370f86fcae5d23d5bef6
yangfa <yangfa1027@gmail.com>
2025-05-11 21:17:43 +0800

    放置mit协议并发布到pypi

--- readme.md
+++ readme.md
@@ -3,7 +3,11 @@ luna是一个专门用于表格类型数据的机器学习框架
 <img src="./imgs/luna.jpg" width="200" height="150" alt="luna">
 
 **luna是这只小猫的名字，来自华南理工大学大学城校区生活区羽毛球场**
-
+## installation
+```
+conda create -n test_luna python=3.11
+pip install lunax
+```
 ```
 luna/
 │
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "lunax",      #这里是pip项目发布的名称
+    version = "0.0.3",  #版本号，数值大的会优先被pip
+    keywords = ["pip", "tabular data"],			# 关键字
+    description = "A machine learning framework.",	# 描述
+    long_description = "A machine learning framework specifically designed for tabular data processing and analysis.",
+    license = "MIT Licence",		# 许可证
+
+    url = "https://github.com/yangfa-zhang/luna",     #项目相关文件地址，一般是github项目地址即可
+    author = "yangfa-zhang",			# 作者
+    author_email = "yangfa1027@gmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy", "pandas","xgboost","tabulate","python-abc","typing","scikit-learn","optuna","lightgbm","catboost","matplotlib","seaborn"]          #这个项目依赖的第三方库
+)
